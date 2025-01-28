406877bc90d958f4c8255558a044f7637af9b809
anandclicks <puspanand94@gmail.com>
2025-01-28 23:42:38 +0530

    home page one design added

---
+++ src/components/AbotMe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AbotMe = () => {
+  return (
+    <div className='h-[100vh] w-[10vw] bg-amber-200 mx-auto'>AbotMe</div>
+  )
+}
+
+export default AbotMe
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
   const navLinks = [
-    {
+    { id : 1,
       title : "work",
       specificClass : 'activeBtn'
     },
     {
+      id : 2,
       title : "Services",
-      specificClass : ''
+      specificClass : 'notActive'
     },
     {
+      id : 3,
       title : "Contact",
       specificClass : 'activeBtn'
     },
-    {
+    { 
+      id : 4,
       title : "About",
-      specificClass : ''
+      specificClass : 'notActive'
     },
   ]
+  const [navLinkStatus , setnavLinkStatus ] = useState(true)
+  const functionForChangingBg = ()=> {
+    navLinks.forEach((item)=> {
+      if(navLinkStatus) {
+        if(item.id % 2 == 0) {
+          item.specificClass = 'notActive'
+        }
+        else {
+          item.specificClass = 'activeBtn'
+        }
+      }
+      else {
+        if(item.id % 2 != 0) {
+          item.specificClass = 'notActive'
+        }
+        else {
+          item.specificClass = 'activeBtn'
+        }
+      }
+    })
+  }
+  
+  functionForChangingBg()
   return (
     <div className='h-[100px] w-full flex justify-between items-end px-4'>
       <img className='absolute mb-7 ms-[350px] w-[140px] start' src="/start.png" alt="" />
@@ -28,10 +54,9 @@ const Navbar = () => {
       </div>
       {/* links  */}
       <div className='navLinks h-[40px] w-[500px] border-[1px] border-stone-400 rounded-3xl flex items-center justify-between px-0 bg-black'>
-        <a className='activeBtn primaryBg text-black px-6 py-2 rounded-3xl h-full flex items-center justify-center' href="">Work</a>
-        <a className=' text-white px-6 py-2 rounded-3xl h-full flex items-center justify-center' href="">Services</a>
-        <a className='activeBtn primaryBg text-black px-6 py-2 rounded-3xl h-full flex items-center justify-center' href="">Contact</a>
-        <a className=' text-white px-6 py-2 rounded-3xl h-full flex items-center justify-center' href="">About</a>
+        {navLinks.map((item,index)=> (
+           <a key={index} className={`${item.specificClass} text-black px-6 py-2 rounded-3xl h-full flex items-center justify-center`} href="">{item.title}</a>
+        ))}
       </div>
     </div>
   )
